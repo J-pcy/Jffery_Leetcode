ac50344d3c242fc51fd48804ff417cfc5a263e6c
Chenyu <chenyupe@usc.edu>
2018-06-28 09:18:17 -0700

    06/28/2018

---
+++ Medium/535_EncodeandDecodeTinyURL.py
@@ -0,0 +1,32 @@
+"""
+TinyURL is a URL shortening service where you enter a URL such as https://leetcode.com/problems/design-tinyurl and it returns a short URL such as http://tinyurl.com/4e9iAk.
+
+Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
+"""
+
+class Codec:
+    def __init__(self):
+        self.index = 0
+        self.map = {}
+
+    def encode(self, longUrl):
+        """Encodes a URL to a shortened URL.
+        
+        :type longUrl: str
+        :rtype: str
+        """
+        self.index += 1
+        self.map[self.index] = longUrl
+        return "http://tinyurl.com/" + str(self.index)
+
+    def decode(self, shortUrl):
+        """Decodes a shortened URL to its original URL.
+        
+        :type shortUrl: str
+        :rtype: str
+        """
+        return self.map[int(shortUrl.split('/')[-1])]
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(url))
